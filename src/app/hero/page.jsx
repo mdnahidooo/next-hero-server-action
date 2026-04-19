3cb7a5504108aad198e4ed22784b997755451324
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-20 01:24:50 +0600

    Using HeroUI components with fetched data (cards, lists)

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero page</h2>
+            <Button>Hero button</Button>
+            <Button variant="secondary">Hero button</Button>
+            <Button variant="outline">Hero button</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,9 +1,18 @@
+import TasksCard from '@/components/TasksCard';
+import { getTasks } from '@/lib/tasks';
 import React from 'react';
 
-const TasksPage = () => {
+const TasksPage = async () => {
+    const tasks =await getTasks();
     return (
         <div>
-           <h2>Tasks:  </h2> 
+            <h2>Tasks: {tasks.length} </h2> 
+            
+            <div className='grid grid-cols-3 gap-4 pt-8'>
+                {
+                    tasks.map(task => <TasksCard key={task.id} task={task}></TasksCard>)
+                }
+            </div>
         </div>
     );
 };
