8beea2fa04e1509db9083292d1c49b3b55c4c6f7
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-20 10:03:21 +0600

    Revalidation after server action to display form data

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,7 +1,26 @@
-export const createATask = async(formData) => {
+import { revalidatePath } from "next/cache";
+import { postTask } from "./tasks";
+
+export const createATask = async (formData) => {
     'use server'
 
-    const name = formData.get('name');
-    console.log("Add a task with name: ", name);
-    console.log("Add a form task: ", formData);
+    // const title = formData.get('title');
+    // const description = formData.get('description');
+    // const priority = formData.get('priority');
+    // const status = formData.get('status');
+    // const assignedTo = formData.get('assignedTo');
+
+    // const newTask = { title, description, priority, status, assignedTo };
+
+    //  uporer part er another way 
+    const newTask = Object.fromEntries(formData.entries());
+    console.log("Add a form task: ", newTask);
+
+    const res = await postTask(newTask)
+
+    if (res.ok) {                         //ok come from tasks.js lib folder
+        revalidatePath('/tasks');
+    }
+
+    return res;
 }
